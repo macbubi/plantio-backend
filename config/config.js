8b18fb0d76d1fec2186fa7fa2debf4a34f1c8098
Daniel Macbubi <daniel.macbubi@zoominfo.com>
2018-12-29 20:58:57 +0200

    add env configuration

--- config/config.js
+++ config/config.js
@@ -1,8 +1,29 @@
-const commonConfig = {
-  mongodbURI: "mongodb://localhost/plantio"
+const devConfig = require('./config.dev');
+const stagingConfig = require('./config.staging');
+const productionConfig = require('./config.production');
+let envConfig;
+
+let commonConfig = {
+
+};
+
+
+function getConfig(env) {
+  switch (env) {
+    case 'production':
+      envConfig = productionConfig
+      break;
+    case 'staging':
+      envConfig = stagingConfig
+      break;
+    default:
+      envConfig = devConfig
+  }
+  
+  return {
+    ...commonConfig,
+    ...envConfig
+  }
 }
 
-module.exports = {
-  // ...envConfig,
-  ...commonConfig
-}
+module.exports = getConfig(process.env.NODE_ENV);
---
+++ config/config.production.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongodbURI: "mongodb://production/plantio"
+}
